dcafa25f67aee84b545963d472737b0910599e67
Shelter C. <noelanischipa@gmail.com>
2025-03-31 17:40:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("GitHub Profile Page Loaded!");
+
+    document.querySelector(".project-card").addEventListener("click", function () {
+        alert("Project Details Coming Soon!");
+    });
+});
